7b3c72bf3becda0e5c898da68d41e8bc3731cbd4
John Wu <johnwu93@gmail.com>
2017-11-19 08:24:49 -0800

    Created utility functions to process image text

---
+++ python/src/model/search/__init__.py
@@ -0,0 +1,1 @@
+from .preprocessing_util import read_image_text, clean_text
---
+++ python/src/model/search/preprocessing_util.py
@@ -0,0 +1,17 @@
+import pytesseract
+from PIL import Image
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
+
+def read_image_text(image_path: str):
+    return pytesseract.image_to_string(Image.open(image_path))
+
+
+def clean_text(sentence):
+    sentence = sentence.lower()
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(sentence)
+    english_stop_words = stopwords.words('english')
+    filtered_words = [w for w in tokens if w not in english_stop_words]
+    return " ".join(filtered_words)
