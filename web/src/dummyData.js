279cd43c5f08b60188e26375b3b2d2c1c955878d
Kay K <ds.kay.kim@gmail.com>
2017-11-19 15:28:34 -0800

    presentation

---
+++ web/src/dummyData.js
@@ -0,0 +1,119 @@
+var generate_meme_data = function (memeImage, text, url) {
+  return {
+    memeImage: memeImage,
+    text: text,
+    url: url
+  };
+};
+
+var trump = [
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/23509334_1737567356294242_2588466208588285552_o.jpg?oh=b74852291b0112b7f0abf651ecbae16e&oe=5A8E0986',
+      'Trump reacts only',
+      'https://www.facebook.com/groups/163576114113950/permalink/357288034742756/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/23561353_891874377646262_458175198508755016_n.jpg?oh=a83160f96937c5f494857af7d7e1da0d&oe=5A8B513A',
+      'The DAILY BRUIN is FAKE NEWS',
+      'https://www.facebook.com/groups/163576114113950/permalink/358078411330385/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/23622265_2150386644987138_386824643883489629_n.jpg?oh=33546973c640e9a298266948c48e5011&oe=5A9E532C',
+      'He’s back at it again lol 😂',
+      'https://www.facebook.com/groups/163576114113950/permalink/357964861341740/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/15337521_10206021971787467_2358107388214023452_n.jpg?oh=78366d9a8b1bfecfaa525d1d5c920dd7&oe=5AAE1AB0',
+      '**BREAKING NEWS**',
+      'https://www.facebook.com/groups/163576114113950/permalink/175417242929837/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/16649362_1329373707124910_240772444703838735_n.jpg?oh=e4ef73bbbeb70de54d9c1f49cb69efa0&oe=5A970BCD',
+      'Condolences to the out-of-staters',
+      'https://www.facebook.com/groups/163576114113950/permalink/223069534831274/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/15844518_1376252382386951_1091299156910423636_o.jpg?oh=8f40d3cf0a9d0bd01f72453b0484b286&oe=5AA7436B,',
+      'Did you all see the poster that Bernie took to the floor of the Senate today?',
+      'https://www.facebook.com/groups/163576114113950/permalink/196042484200646/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/16427328_1292237620838763_3441926405816530603_n.jpg?oh=a215cb9b256b069d582589975f69d84e&oe=5A8C8AF7',
+      'He is completely out of control.\n',
+      'https://www.facebook.com/groups/163576114113950/permalink/215865628884998/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/16864559_1223563254358744_4630753109360231714_n.jpg?oh=0e4956d2565115ce5d133d02808cd137&oe=5A8F1870',
+      '1st year Premium Plan Reax only please 😥\n',
+      'https://www.facebook.com/groups/163576114113950/permalink/227935584344669/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/18892975_1388074301271749_970781289126822925_n.jpg?oh=5d117fc654e522c6d3ec8f9ac1223301&oe=5A93C19C',
+      'The real story behind #Covfefe\n',
+      'https://www.facebook.com/groups/163576114113950/permalink/284123255392568/'
+    ),
+    generate_meme_data(
+      'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/22448261_875282429305457_5983581070088474124_n.jpg?oh=ca05bd94879902a38d950e31cf3bb20a&oe=5A94A2EA',
+      'Sad!',
+      'https://www.facebook.com/groups/163576114113950/permalink/343271649477728/'
+    )
+  ]
+;
+
+var bananas = [
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/23550350_1739551089423303_1133020351315796914_o.jpg?oh=4f976763e2091a73121ba9b6e1598fc0&oe=5A9690D2',
+    'Bananas don\'t lie',
+    'https://www.facebook.com/groups/163576114113950/permalink/357308374740722/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/22552559_10210161708465979_6612045437955191606_n.jpg?oh=95bb9c08f27ea079c9f2639c0fe2b327&oe=5A9755B2',
+    '',
+    'https://www.facebook.com/groups/163576114113950/permalink/348074108997482/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/23032392_541417886200030_8165332343520477906_n.jpg?oh=99174d2b2de8e43954ef84e6ffbce842&oe=5A900A2A',
+    'When deciding where to study abroad',
+    'https://www.facebook.com/groups/163576114113950/permalink/352918411846385/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/23561648_2008589332754585_3062220385616837438_n.jpg?oh=5f0418510864970fb4313e64c2e62982&oe=5A8CDBAE',
+    'a Bplate adventure',
+    'https://www.facebook.com/groups/163576114113950/permalink/358172577987635/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/22904918_10155760969616168_7442391891378480509_o.jpg?oh=50008853f882046873a494d938b29800&oe=5A93D9FB',
+    'Also please comment: thaaaaaankyou blessed bruin banana \nfor extra luck during midterms my guys',
+    'https://www.facebook.com/groups/163576114113950/permalink/350578818747011/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/20368773_1365502716878318_450094862219065929_o.jpg?oh=d1cbc7c598dc37b2c9bee5cec641988e&oe=5A98C525',
+    'Nothing can stand between a True Bruin and dining hall bananas',
+    'https://www.facebook.com/groups/163576114113950/permalink/312168682588025/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/15337562_221548558288309_1897707631257476281_n.jpg?oh=704057149f021d66e1359d186d91567c&oe=5A958C72',
+    '',
+    'https://www.facebook.com/groups/163576114113950/permalink/175688592902702/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/21743284_1458671290880672_6137085637693465716_n.jpg?oh=1905de12f9aa0aa9b29481ac85d9dba5&oe=5A92D88F',
+    'That one friend who always takes a banana but never actually eats it.',
+    'https://www.facebook.com/groups/163576114113950/permalink/332950660509827/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/18839233_1296854383717448_3728706593007434625_n.jpg?oh=406b9c6458d2e916272f342c45b2d2ce&oe=5A9F11B2',
+    'admiring your hoard of bananas smuggled from the dining halls like',
+    'https://www.facebook.com/groups/163576114113950/permalink/285456548592572/'
+  ),
+  generate_meme_data(
+    'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/16729497_10212441111129944_9100374899424957532_n.jpg?oh=9c012078e07a1b59c63f556712250925&oe=5A9CC619',
+    'When you sneak some bananas for yourself and your buddy',
+    'https://www.facebook.com/groups/163576114113950/permalink/220329921771902/'
+  )
+];
+
+var dummyData = {
+  bananas: bananas,
+  trump: trump
+}
--- web/src/popup.js
+++ web/src/popup.js
@@ -1,89 +1,62 @@
 $(document).ready(function () {
-  const memeUrl = 'https://api.imgflip.com/get_memes'
-  function getMemes() {
-    return new Promise(function (resolve, reject) {
-      fetch(memeUrl)
-        .then(response => response.json())
-        .then(data => { console.log(data); resolve(data) })
-    })
-  }
+  // TESTING PURPOSE
+  // const memeUrl = 'https://api.imgflip.com/get_memes'
+  // function getMemes() {
+  //   return new Promise(function (resolve, reject) {
+  //     fetch(memeUrl)
+  //       .then(response => response.json())
+  //       .then(data => { console.log(data); resolve(data) })
+  //   })
+  // }
 
-  (function getUrl(callback) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      url = tabs[0].url;
-      callback(url)
-    });
-  })(console.log)
+  // (function getUrl(callback) {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //     url = tabs[0].url;
+  //     callback(url)
+  //   });
+  // })(console.log)
 
   var $divParent = $('<div>')
   $divParent.addClass('parent')
   $('html').removeClass('no-js')
 
-  // getMemes().then(function (data) {
-  //   var memes = data.data.memes
-  //   for (var i = 0; i < 12; i++) {
-  //     $initialDivChild = $('<div>')
-  //     $p = $('<p>')
-  //     memes[i]
-  //     var name = memes[i].name
-  //     var url = memes[i].url
-  //     var width = memes[i].width
-  //     var height = memes[i].height
-  //     $p.text(name)
-  //     var $img = $('<img>')
-  //     $img.attr({ name: name, src: url, width: 220, href: url })
-  //     $img.click(function () {
-  //       chrome.tabs.create({ url: $(this).attr('href') })
-  //     })
-  //     var $imgWrap = $('<div>')
-  //     $imgWrap.addClass('imageWrapper')
-  //     $initialDivChild.append($p)
-  //     $imgWrap.append($img)
-  //     $initialDivChild.append($imgWrap)
-  //     $initialDivParent.append($initialDivChild)
-  //   }
-  //   $('#initial-memes').append($initialDivParent)
+  // WIP for additional tab  
+  // $('#show-universal').on('click', function () {
+  //   $('#facebook-feed').show()
+  //   $('#universal-feed').hide()
   // })
 
-  $('#show-universal').on('click', function () {
-    $('#facebook-feed').show()
-    $('#universal-feed').hide()
-  })
-  $('#show-fb').on('click', function () {
-    $('#facebook-feed').hide()
-    $('#universal-feed').show()
-  })
+  // $('#show-fb').on('click', function () {
+  //   $('#facebook-feed').hide()
+  //   $('#universal-feed').show()
+  // })
+
   $('#search').on('change', function (e) {
     $('.divChild').remove();
     searchWord = document.getElementsByName('search')[0].value.toLowerCase();
-    getMemes().then(function (data) {
-      var memes = data.data.memes;
-      for (let i = 0; i < memes.length; i++) {
-        if (memes[i].name.toLowerCase().includes(searchWord)) {
-          $divChild = $('<div>')
-          $divChild.addClass('divChild')
-          $p = $('<p>')
-          memes[i]
-          var name = memes[i].name
-          var url = memes[i].url
-          var width = memes[i].width
-          var height = memes[i].height
-          $p.text(name)
-          var $img = $('<img>')
-          $img.attr({ name: name, src: url, width: 200, href: url })
-          $img.click(function () {
-            chrome.tabs.create({ url: $(this).attr('href') })
-          })
-          var $imgWrap = $('<div>')
-          $imgWrap.addClass('imageWrapper')
-          $divChild.append($p)
-          $imgWrap.append($img)
-          $divChild.append($imgWrap)
-          $divParent.append($divChild)
-          $('#memes').append($divParent)
-        }
-      }
-    })
+    if (searchWord in dummyData) {
+      dummyData[searchWord].forEach(function (meme) {
+        $divChild = $('<div>')
+        $divChild.addClass('divChild')
+        $p = $('<p>')
+        var text = meme.text
+        var imageUrl = meme.memeImage
+        var fbLink = meme.url
+        $p.text(text)
+        var $img = $('<img>')
+        $img.attr({ name: text, src: imageUrl, width: 200, height: 200, href: fbLink })
+        $img.click(function () {
+          chrome.tabs.create({ url: $(this).attr('href') })
+        })
+        var $imgWrap = $('<div>')
+        $imgWrap.addClass('imageWrapper')
+        $divChild.append($p)
+        $imgWrap.append($img)
+        $divChild.append($imgWrap)
+        $divParent.append($divChild)
+        $('#memes').append($divParent)
+      })
+    }
   })
 })
 
